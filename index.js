bde146c8e126ef458b88a9079457064cbef817b3
Alvaro Maceda <alvaro@alvaromaceda.es>
2022-02-02 17:02:03 +0100

    adds CORS

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const DEFAULT_PORT = 3000
 const PORT = process.env.PORT || DEFAULT_PORT // Heroku assigns you a port
 
 import express from 'express'
+import cors from 'cors'
 import * as validateRoute from './routes/validate.js'
 
 const app = express()
 app.use(express.urlencoded())
+const corsOptions = {
+  origin: '*'
+}
+app.use(cors(corsOptions));
 
 validateRoute.install(app)
 
