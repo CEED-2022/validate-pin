b2cca541f8ab88467f1b45b4b7b24d0021bffb03
Alvaro Maceda <alvaro@alvaromaceda.es>
2022-02-02 16:56:42 +0100

    removes unnecesary returns

--- routes/validate.js
+++ routes/validate.js
@@ -3,21 +3,18 @@ function validateContentType(req, res, next) {
   if (req.is('application/x-www-form-urlencoded')) return next();
 
   res.status(400).send('Invalid content-type');
-  return false;
 }
 
 function validateFieldNumber(req, res, next) {
   if(Object.keys(req.body).length === 1) return next();
 
   res.status(400).send('Expected a single field "PIN"')
-  return false;
 }
 
 function validatePinLength(req, res, next) {
   if(req.body?.PIN?.length === 5) return next();
 
   res.status(400).send('Expected a five digit PIN')
-  return false;
 }
 
 function handler(req, res) {
